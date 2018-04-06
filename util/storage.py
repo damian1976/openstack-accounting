db6cedd53c6565a26097d74fac9ecb31aaffcc3c
Damian Kaliszan <damian@man.poznan.pl>
2018-04-06 11:26:45 +0200

    Comments removed

--- util/storage.py
+++ util/storage.py
@@ -2,7 +2,8 @@ from .os_data import AccountData
 
 
 # 'Server' class
-# Contains all server accounting data
+# Contains all Storage (Volume/Image)
+# accounting data
 class Storage(AccountData):
     def __init__(self,
                  name,
@@ -20,7 +21,7 @@ class Storage(AccountData):
         self.project_name = ''
         self.typename = typename
 
-    # Returns String representation for a server object
+    # Returns String representation for a storage object
     def __str__(self):
         str = "{0} name: {1} ({2})\n" \
               "\tHours: {3:.2f}\n" \
@@ -44,50 +45,3 @@ class Storage(AccountData):
             print("Error {0}".format(e))
             return 0.0
         return self.total_cost
-'''
-
-    # Updates server flavors with STOP. SHELVE statuses from config
-    def updateHoursAndVolumes(self,
-                              stop_timeframes,
-                              shelve_timeframes,
-                              delete_timeframes,
-                              coeff,
-                              ):
-        if delete_timeframes:
-            for hours in delete_timeframes:
-                self.hrs -= hours
-                self.disk['hours'] -= self.disk['value']*hours
-                self.cpu['hours'] -= self.cpu['value']*hours
-                self.ram['hours'] -= self.ram['value']*hours
-        if stop_timeframes:
-            for hours in stop_timeframes:
-                self.hrs -= hours*(1.0 - coeff['stop'])
-                self.cpu['hours'] -=\
-                    self.cpu['value']*hours*(1.0 - coeff['stop_cpu'])
-                self.ram['hours'] -=\
-                    self.ram['value']*hours*(1.0 - coeff['stop_ram'])
-                self.disk['hours'] -=\
-                    self.disk['value']*hours*(1.0 - coeff['stop_disk'])
-        if shelve_timeframes:
-            for hours in shelve_timeframes:
-                self.hrs -= hours*(1.0 - coeff['shelve'])
-                self.cpu['hours'] -=\
-                    self.cpu['value']*hours*(1.0 - coeff['shelve_cpu'])
-                self.ram['hours'] -=\
-                    self.ram['value']*hours*(1.0 - coeff['shelve_ram'])
-                self.disk['hours'] -=\
-                    self.disk['value']*hours*(1.0 - coeff['shelve_disk'])
-        if (self.hrs == 0.0):
-            self.cpu['hours'] = self.ram['hours'] = self.disk['hours'] = 0.0
-
-    # Updates server flavors with ACTIVE status coefficients from config
-    def updateMetricHoursWithActiveStatus(self, coeff):
-        if (not coeff):
-            return
-        self.hrs *= coeff['active']
-        self.disk['hours'] *= coeff['active_disk']
-        self.cpu['hours'] *= coeff['active_cpu']
-        self.ram['hours'] *= coeff['active_ram']
-
-    # Returns total cost for a server
-'''
